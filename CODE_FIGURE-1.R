0106f714ffa05544f96d9f2e50c34d7041a3565c
aditibagchi <aditi.bagchi@vai.org>
2019-01-31 16:57:18 -0500

    COMMIT_13119

---
+++ CODE_FIGURE-1.R
@@ -0,0 +1,51 @@
+##Tumor Mutational Burden in Primary and Recurrent Medulloblastoma; Calculation of Outliers
+Medulloblastoma <- read.csv("/Volumes/G-DRIVE mobile/Data_Analysis/Thesis/Thesis/ALL_SAMPLES_TMD.csv", comment.char="#")
+str(Medulloblastoma)
+head(Medulloblastoma)
+hist(Medulloblastoma$Total.Mutation.Burden)
+##The histogram is a skewed one the majority of the data lies between 1-2; to geta better judegement of the outliers
+## Use boxplot function to get a real visual of outliers. 
+boxplot(Medulloblastoma$Total.Mutation.Burden, ylab = 'Mutation per Mb')
+## 9 outliers noted 
+## When is a value an outlier: *expert judgement, *Rule of thumb: Q1-1.5 *IQR 
+                                                                  Q3 + 1.5*IQR  [ IQR ~ Interquartile range]
+                                                                  
+ ## Calculate IQR for the tumor mutational burdden
+IQR(Medulloblastoma$Total.Mutation.Burden) 
+##1.046815
+quantile(Medulloblastoma$Total.Mutation.Burden)
+##0%        25%        50%        75%       100% 
+##0.2102102(Q1)  1.1003317(Q2)  1.4029851(Q3)  2.1471471(Q4) 39.4594595 
+
+##Outliers  are >  2.14(Q3) + 1.5*1.04(IQR)
+2.14 + (1.5*1.04)
+3.7
+##Any tumor with tumor mutational burden of more than 3.7 can be considered as out liers.
+
+Medulloblastoma1 <- Medulloblastoma[ which(Medulloblastoma$Total.Mutation.Burden < 3.7),]
+Medulloblastoma2 <- Medulloblastoma[ which(Medulloblastoma$Total.Mutation.Burden > 3.7),]
+Medulloblastoma2
+
+boxplot(Medulloblastoma1$Total.Mutation.Burden, ylab = "Mutaion per MB")
+median(Medulloblastoma1$Total.Mutation.Burden)
+[1] 1.377258
+mean(Medulloblastoma1$Total.Mutation.Burden)
+[1] 1.450055
+## Now make TMD plot
+## All tumors with Mutational burden more than 3.7 mutation per Mb are considered hypermutated. A total of 9 samples. 
+## Two graphs are created for tumor mutational burden.
+
+ggplot2::
+ggthemes::
+scales::
+ggrepel::
+RColorBrewer::
+grid::
+
+X <- ggplot(Medulloblastoma, aes(Medulloblastoma$Name, Medulloblastoma$Total.Mutation.Burden))
+Y <- X + geom_bar(stat = "identity", aes(fill = TYPE))+ 
+  scale_y_continuous(name="Mutations per Mb", limits=c(0, 40)) + theme_minimal() + xlab("Tumor")+
+  scale_fill_brewer(palette = "Set1") + theme(axis.text.x = element_text(color = "black", size = 8, angle = 90))
+
+
+
